c3f8aabe3d9f29f5847b7e4761ead7df3023c6c6
Rafael Bento <109679117+RafaelBento7@users.noreply.github.com>
2022-11-15 19:59:57 +0000

    Correcoes template (#15)
    * Adicionar navegação pela navbar
    
    * Create Employee e Update Employee
    
    * SideBar Nome de utilizador
    
    * Correção de bug do criar Employee
    
    * Roles, Permissions e Rules RBAC
    
    * Employee Controller com AccessControl
    
    * Pequenas correções de layout
    
    * Página Index atualizada de acordo com as roles RBAC
    
    * Correção employee create
    
    * update navbar loggedin e correçoes css
    
    Co-authored-by: Pedro Henriques <pedrohenriques888@live.com>

--- aerocontrol/frontend/views/layouts/main.php
+++ aerocontrol/frontend/views/layouts/main.php
@@ -33,8 +33,6 @@ AppAsset::register($this);
 </head>
 <body class="body-grid">
 <?php $this->beginBody() ?>
-
-
     <header class="[ primary-header ] [ padding-block-100 bg-neutral-400 ]">
         <div class="container">
             <div class="nav-wrapper">
@@ -44,57 +42,96 @@ AppAsset::register($this);
                         <img src="<?= Url::to('@web/images/logo-mobile.svg' )?>" alt="Logo">
                     </picture>
                 </a>
-                <button class="[ navbar-toggle ] [ d-none-md push-to-right ] " aria-controls="primary-navigation"
-                        aria-expanded="false">
+                <button class="[ navbar-toggle ] [ d-none-md push-to-right ] " aria-controls="primary-navigation" aria-expanded="false">
                     <img class="open-icon" src="<?= Url::to('@web/images/hamburger-icon.svg' )?>" alt="" aria-hidden="true">
                     <img class="close-icon" src="<?= Url::to('@web/images/close-icon.svg' )?>" alt="" aria-hidden="true">
                     <span class="visually-hidden">Menu</span>
                 </button>
-                <nav aria-label="Primary" class="primary-navigation" id="primary-navigation">
-                    <ul role="list" class="navigation-list">
-                        <li class="[ primary-navigation__item ] [ push-to-right ]" data-type="active">
-                            <a href="<?= Url::to(['site/index'])?>" class="[ primary-navigation__link ] [ fs-300 ]">Home</a>
-                        </li>
-                        <li class="primary-navigation__item">
-                            <a href="<?= Url::to(['site/voos'])?>" class="[ primary-navigation__link ] [ fs-300 ]">Voos</a>
-                        </li>
-                        <li class="primary-navigation__item">
-                            <a href="<?= Url::to(['site/restaurantes'])?>" class="[ primary-navigation__link ] [ fs-300 ]">Restaurantes</a>
-                        </li>
-                        <li class="[ primary-navigation__item ] [ push-to-left ]">
-                            <a href="<?= Url::to(['site/lojas'])?>" class="[ primary-navigation__link ] [ fs-300 ]">Lojas</a>
-                        </li>
-                        <li class="primary-navigation__item">
-                            <a href="<?= Url::to(['site/signup'])?>" class="button" data-type="primary-outline">Sign Up</a>
-                        </li>
-                        <li class="primary-navigation__item"><a href="<?= Url::to(['site/login'])?>" class="button">Login</a></li>
-                        <?= Html::a('<i class="fas fa-sign-out-alt">logout</i>', ['/site/logout'], ['data-method' => 'post', 'class' => 'nav-link']) ?>
-                    </ul>
-                </nav>
+                <?php if(Yii::$app->user->isGuest):?>
+                    <nav aria-label="Primary" class="primary-navigation" id="primary-navigation">
+                        <ul role="list" class="navigation-list">
+                            <li class="[ primary-navigation__item ] [ push-to-right ]" data-type="active">
+                                <a href="<?= Url::to(['site/index'])?>" class="[ primary-navigation__link ] [ fs-300 ]">Home</a>
+                            </li>
+                            <li class="primary-navigation__item">
+                                <a href="<?= Url::to(['site/voos'])?>" class="[ primary-navigation__link ] [ fs-300 ]">Voos</a>
+                            </li>
+                            <li class="primary-navigation__item">
+                                <a href="<?= Url::to(['site/restaurantes'])?>" class="[ primary-navigation__link ] [ fs-300 ]">Restaurantes</a>
+                            </li>
+                            <li class="[ primary-navigation__item ] [ push-to-left ]">
+                                <a href="<?= Url::to(['site/lojas'])?>" class="[ primary-navigation__link ] [ fs-300 ]">Lojas</a>
+                            </li>
+                            <li class="primary-navigation__item">
+                                <a href="<?= Url::to(['site/signup'])?>" class="button" data-type="primary-outline">Sign Up</a>
+                            </li>
+                            <li class="primary-navigation__item"><a href="<?= Url::to(['site/login'])?>" class="button">Login</a></li>
+                        </ul>
+                    </nav>
+                <?php else: ?>
+                        <nav aria-label="Primary" class="primary-navigation" id="primary-navigation">
+                            <ul role="list" class="navigation-list">
+                                <li class="[ primary-navigation__item ] [ push-to-right ]" data-type="active"><a href="#"class="[ primary-navigation__link ] [ fs-300 ]">Home</a></li>
+                                <li class="primary-navigation__item"><a href="#"class="[ primary-navigation__link ] [ fs-300 ]">Voos</a></li>
+                                <li class="primary-navigation__item"><a href="#"class="[ primary-navigation__link ] [ fs-300 ]">Restaurantes</a></li>
+                                <li class="[ primary-navigation__item ] [ push-to-left ]"><a href="#"class="[ primary-navigation__link ] [ fs-300 ]">Lojas</a></li>
+                                <li class="primary-navigation__item d-flex justify-content-center">
+                                    <div class="dropdown " data-type="navbar">
+                                        <button class="dropdown-button button" data-type="primary-outline" aria-expanded="false" data-dropdown>
+                                            <?=\Yii::$app->user->identity->first_name." ".Yii::$app->user->identity->last_name ?>
+                                            <span aria-hidden="true">
+                                                <svg class="icon dropdown__toggle-icon">
+                                                    <use xlink:href="<?=Url::to('@web/images/caret.svg#caret')?>"></use>
+                                                </svg>
+                                            </span>
+                                        </button>
+                                        <ul role="list" class="dropdown-menu">
+                                            <li class="dropdown-menu__item">
+                                                <a class="[ dropdown-menu__link ] [ text-primary-accent-400 ]" href="#"> Ver conta
+                                                    <span aria-hidden="true">
+                                                        <svg class="icon dropdown-link__icon">
+                                                            <use xlink:href="<?=Url::to('@web/images/perfil-icon.svg#perfil-icon')?>"></use>
+                                                        </svg>
+                                                    </span>
+                                                </a></li>
+                                            <li class="dropdown-menu__item">
+                                                <?= Html::a('Logout'. '<span aria-hidden="true">
+                                                        <svg class="icon dropdown-link__icon">
+                                                            <use xlink:href="'.Url::to('@web/images/logout-icon.svg#logout-icon').'"></use>
+                                                        </svg>
+                                                    </span>', ['site/logout'], ['data-method' => 'post', 'class' => '[ dropdown-menu__link ] [ text-primary-accent-400 ]']) ?>
+
+                                                </a>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </li>
+                            </ul>
+                        </nav>
+                <?php endif; ?>
             </div>
         </div>
     </header>
 
+    <?= $content ?>
 
-        <?= $content ?>
-
-<footer >
-    <div class="[ footer-wrapper ] [ container text-align-center ]">
-        <nav aria-label="Footer">
-            <ul role="list" class="footer-list">
-                <li class="footer-list__item"><a href="#"
-                                                 class="[ footer__link ] [ fs-350 letter-spacing-2 ]">Termos e Condições</a></li>
-                <li class="footer-list__item"><a href="#"
-                                                 class="[ footer__link ] [ fs-350 letter-spacing-2 ]">Política de privacidade</a>
-                </li>
-                <li class="footer-list__item"><a href="#"
-                                                 class="[ footer__link ] [ fs-350 letter-spacing-2 ]">Contactar Suporte</a></li>
-            </ul>
-        </nav>
-        <p class="fs-100 fw-light letter-spacing-2 ">@ 2022 AeroControl. Todos os direitos
-            reservados.</p>
-    </div>
-</footer>
+    <footer >
+        <div class="[ footer-wrapper ] [ container text-align-center ]">
+            <nav aria-label="Footer">
+                <ul role="list" class="footer-list">
+                    <li class="footer-list__item"><a href="#"
+                                                     class="[ footer__link ] [ fs-350 letter-spacing-2 ]">Termos e Condições</a></li>
+                    <li class="footer-list__item"><a href="#"
+                                                     class="[ footer__link ] [ fs-350 letter-spacing-2 ]">Política de privacidade</a>
+                    </li>
+                    <li class="footer-list__item"><a href="#"
+                                                     class="[ footer__link ] [ fs-350 letter-spacing-2 ]">Contactar Suporte</a></li>
+                </ul>
+            </nav>
+            <p class="fs-100 fw-light letter-spacing-2 ">@ 2022 AeroControl. Todos os direitos
+                reservados.</p>
+        </div>
+    </footer>
 
 <?php $this->endBody() ?>
 </body>
