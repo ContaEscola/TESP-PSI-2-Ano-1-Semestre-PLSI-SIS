4147332e75d399a1b6fae699f4a35b89a007ecd6
pedrohenriques888@live.com <pedrohenriques888@live.com>
2022-11-15 16:37:57 +0000

    Conclusão do formulario de signup

--- aerocontrol/frontend/views/site/signup.php
+++ aerocontrol/frontend/views/site/signup.php
@@ -11,7 +11,7 @@ $this->title = 'Signup';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <main>
-    <?php $form = ActiveForm::begin(['id' => 'login-form']) ?>
+    <?php $form = ActiveForm::begin(['id' => 'signup-form']) ?>
     <div class="padding-block-700 height-100 d-grid align-items-center">
         <div class="container" data-type="small-md">
             <div class="text-align-center flow" data-flow-space="small">
@@ -19,7 +19,6 @@ $this->params['breadcrumbs'][] = $this->title;
                 <p>Crie a sua conta para começar a reservar voos e muito mais.</p>
             </div>
             <div class="margin-top-600 flow" data-flow-space="large">
-                <?php $form = ActiveForm::begin(['id' => 'login-form']) ?>
                 <div>
                     <h2 class="fs-500 fw-semi-bold text-align-center-sm">Dados de acesso</h2>
                     <div class="even-columns gap-2-sm gap-1-md margin-top-200">
@@ -51,7 +50,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     <div class="flow margin-top-200">
                         <div class="even-columns gap-2-sm gap-1-md ">
                             <div class="form__group">
-                                <label for="username" class="[ input__label ] [ margin-bottom-50 ]">Primeiro Nome:</label>
+                                <label for="first_name" class="[ input__label ] [ margin-bottom-50 ]">Primeiro Nome:</label>
                                 <?= $form->field($model,'first_name', [
                                     'options' => ['class' => 'form-group has-feedback'],
                                     'template' => '{beginWrapper}{input}{error}{endWrapper}',
@@ -61,33 +60,54 @@ $this->params['breadcrumbs'][] = $this->title;
                                     ->textInput(['placeholder' => $model->getAttributeLabel('')]) ?>
                             </div>
                             <div class="form__group">
-                                <label for="last-name" class="[ input__label ] [ margin-bottom-50 ]">Último nome:</label>
-                                <input class="form__input" type="text" name="{TOCHANGE}" id="last-name" required>
-                                <p class="[ input__error ] [ margin-top-100 ]"></p>
+                                <label for="last_name" class="[ input__label ] [ margin-bottom-50 ]">Ultimo Nome:</label>
+                                <?= $form->field($model,'last_name', [
+                                    'options' => ['class' => 'form-group has-feedback'],
+                                    'template' => '{beginWrapper}{input}{error}{endWrapper}',
+                                    'wrapperOptions' => ['class' => 'input-group mb-3']
+                                ])
+                                    ->label(false)
+                                    ->textInput(['placeholder' => $model->getAttributeLabel('')]) ?>
                             </div>
                             <div class="form__group">
                                 <label for="gender" class="[ input__label ] [ margin-bottom-50 ]">Género:</label>
-                                <select class="[ form__input ] [ height-100 ]" name="genders" id="gender" required>
-                                    <!-- Get from database -->
-                                </select>
-                                <p class="[ input__error ] [ margin-top-100 ]"></p>
+                                <?= $form->field($model, 'gender', [
+                                    'options' => ['class' => 'form-group has-feedback'],
+                                    'template' => '{beginWrapper}{input}{error}{endWrapper}',
+                                    'wrapperOptions' => ['class' => 'input-group mb-3']
+                                ])
+                                    ->dropDownList([ 'Masculino' => 'Masculino','Feminino' => 'Feminino','Outro' => 'Outro' ]) ?>
                             </div>
                         </div>
                         <div class="d-flex flex-flow-column-sm gap-2">
                             <div class="form__group">
                                 <label for="birthdate" class="[ input__label ] [ margin-bottom-50 ]">Data de Nascimento:</label>
-                                <input class="form__input" type="date" name="{TOCHANGE}" id="birthdate" required>
-                                <p class="[ input__error ] [ margin-top-100 ]"></p>
+                                <?= $form->field($model,'birthdate', [
+                                    'options' => ['class' => 'form-group has-feedback'],
+                                    'template' => '{beginWrapper}{input}{error}{endWrapper}',
+                                    'wrapperOptions' => ['class' => 'input-group mb-3']
+                                ])
+                                    ->Input('date' ) ?>
                             </div>
                             <div class="form__group">
                                 <label for="country" class="[ input__label ] [ margin-bottom-50 ]">País:</label>
-                                <input class="form__input min-width-initial " type="text" name="{TOCHANGE}" id="country" required>
-                                <p class="[ input__error ] [ margin-top-100 ]"></p>
+                                <?= $form->field($model,'country', [
+                                    'options' => ['class' => 'form-group has-feedback'],
+                                    'template' => '{beginWrapper}{input}{error}{endWrapper}',
+                                    'wrapperOptions' => ['class' => 'input-group mb-3']
+                                ])
+                                    ->label(false)
+                                    ->textInput(['placeholder' => $model->getAttributeLabel('')]) ?>
                             </div>
                             <div class="form__group">
                                 <label for="city" class="[ input__label ] [ margin-bottom-50 ]">Cidade:</label>
-                                <input class="form__input" type="text" name="{TOCHANGE}" id="city" required>
-                                <p class="[ input__error ] [ margin-top-100 ]"></p>
+                                <?= $form->field($model,'city', [
+                                    'options' => ['class' => 'form-group has-feedback'],
+                                    'template' => '{beginWrapper}{input}{error}{endWrapper}',
+                                    'wrapperOptions' => ['class' => 'input-group mb-3']
+                                ])
+                                    ->label(false)
+                                    ->textInput(['placeholder' => $model->getAttributeLabel('')]) ?>
                             </div>
                         </div>
                     </div>
@@ -98,14 +118,31 @@ $this->params['breadcrumbs'][] = $this->title;
                     <div class="d-flex flex-flow-column-sm margin-top-200">
                         <div class="form__group flex-1">
                             <label for="email" class="[ input__label ] [ margin-bottom-50 ]">Email:</label>
-                            <input class="form__input" type="email" name="{TOCHANGE}" id="email" required>
-                            <p class="[ input__error ] [ margin-top-100 ]"></p>
+                            <?= $form->field($model,'email', [
+                                'options' => ['class' => 'form-group has-feedback'],
+                                'template' => '{beginWrapper}{input}{error}{endWrapper}',
+                                'wrapperOptions' => ['class' => 'input-group mb-3']
+                            ])
+                                ->label(false)
+                                ->textInput(['placeholder' => $model->getAttributeLabel('')]) ?>
                         </div>
                         <div class="form__group flex-1">
                             <label for="phone-number" class="[ input__label ] [ margin-bottom-50 ]">Contacto telefónico:</label>
                             <div class="d-flex width-100">
-                                <input class="[ form__input ] [ flex-1 ]" type="text" name="{TOCHANGE}" required placeholder="Indicativo">
-                                <input class=" [ form__input ] [ flex-2 ]" type="text" name="{TOCHANGE}" id="phone-number" required placeholder="Número telefone">
+                                <?= $form->field($model,'phone_country_code', [
+                                    'options' => ['class' => 'form-group has-feedback'],
+                                    'template' => '{beginWrapper}{input}{error}{endWrapper}',
+                                    'wrapperOptions' => ['class' => 'input-group mb-3']
+                                ])
+                                    ->label(false)
+                                    ->textInput(['placeholder' => $model->getAttributeLabel('Indicativo')]) ?>
+                                <?= $form->field($model,'phone', [
+                                    'options' => ['class' => 'form-group has-feedback'],
+                                    'template' => '{beginWrapper}{input}{error}{endWrapper}',
+                                    'wrapperOptions' => ['class' => 'input-group mb-3']
+                                ])
+                                    ->label(false)
+                                    ->Input('number', ['placeholder' => $model->getAttributeLabel('Número telefone')]) ?>
                             </div>
                             <p class="[ input__error ] [ margin-top-100 ]"></p>
                         </div>
