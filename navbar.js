4867298541db2c50b47dd36b6ecbed4c2543b0c6
ContaEscola <2211904@my.ipleiria.pt>
2022-11-16 15:11:15 +0000

    Alert fecha automaticamente passado um tempo

--- navbar.js
+++ navbar.js
@@ -1,13 +1,18 @@
 const navbarToggle = document.querySelector(".navbar-toggle");
 const primaryNav = document.querySelector(".primary-navigation");
 
-navbarToggle.addEventListener('click', () => {
+if (navbarToggle !== null) {
 
-    navbarToggle.getAttribute("aria-expanded") === "true"
-        ?
-        navbarToggle.setAttribute("aria-expanded", false)
-        :
-        navbarToggle.setAttribute("aria-expanded", true)
 
-    primaryNav.toggleAttribute("data-visible");
-})
+    navbarToggle.addEventListener('click', () => {
+
+        navbarToggle.getAttribute("aria-expanded") === "true"
+            ?
+            navbarToggle.setAttribute("aria-expanded", false)
+            :
+            navbarToggle.setAttribute("aria-expanded", true)
+
+        primaryNav.toggleAttribute("data-visible");
+    })
+
+}
