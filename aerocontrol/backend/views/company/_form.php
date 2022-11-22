db817a5a686815a12bfbc57239a1ae5842c930b7
pedrohenriques888 <74295605+pedrohenriques888@users.noreply.github.com>
2022-11-22 23:25:33 +0000

    Criar companhias (#25)
    * Listar companhias
    
    * Retirar titulo duplicado
    
    * Alterar a ordem das companhias
    
    * adicionar accessControl
    
    * Fixes nas companhias
    
    * Fixes nas companys
    
    Co-authored-by: RafaelBento7 <rafaelcarreirabento7@gmail.com>
    Co-authored-by: ContaEscola <2211904@my.ipleiria.pt>

---
+++ aerocontrol/backend/views/company/_form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var common\models\Company $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="company-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'state')->dropDownList([ '0' => 'Inativo','1' => 'Ativo']) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Guardar', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
