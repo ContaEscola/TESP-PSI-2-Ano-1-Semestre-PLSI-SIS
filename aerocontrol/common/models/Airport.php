9e71343fcf932a50c276fa8132fd8264047b7c17
ContaEscola <2211904@my.ipleiria.pt>
2022-11-25 19:01:13 +0000

    Fixes nos aeroportos

--- aerocontrol/common/models/Airport.php
+++ aerocontrol/common/models/Airport.php
@@ -32,12 +32,29 @@ class Airport extends \yii\db\ActiveRecord
     public function rules()
     {
         return [
-            [['country', 'city', 'name', 'website'], 'required', 'message' => "{attribute} não pode ser vazio."],
             [['country', 'website', 'city', 'name'], 'trim'],
-            [['country', 'website'], 'string', 'max' => 50,'message' => '{attribute} não pode ser superior a 50 caracteres.'],
-            [['city', 'name'], 'string', 'max' => 75,'message' => '{attribute} não pode ser superior a 75 caracteres.'],
-            ['name', 'unique','targetClass' => '\common\models\Airport','message' => 'Já existe um aeroporto com esse nome.'],
-            ['website', 'unique','targetClass' => '\common\models\Airport','message'=> 'Já existe um aeroporto com esse website.'],
+            [['country', 'city', 'name', 'website'], 'required', 'message' => "{attribute} não pode ser vazio."],
+
+            ['country', 'trim'],
+            ['country', 'required', 'message' => "É necessário o país."],
+            [
+                'country', 'string',
+                'min' => 4, 'tooShort' => 'O nome do país deve conter pelo menos 4 caracteres.',
+                'max' => 50, 'tooLong' => 'O nome do país não pode exceder os 50 caracteres.'
+            ],
+
+            ['city', 'trim'],
+            ['city', 'required', 'message' => "É necessário a cidade."],
+            [
+                'city', 'string',
+                'min' => 1, 'tooShort' => 'O nome da cidade deve conter pelo menos 1 caractere.',
+                'max' => 75, 'tooLong' => 'O nome da cidade não pode exceder os 75 caracteres.'
+            ],
+
+            [['website'], 'string', 'max' => 50, 'message' => '{attribute} não pode ser superior a 50 caracteres.'],
+            [['name'], 'string', 'max' => 75, 'message' => '{attribute} não pode ser superior a 75 caracteres.'],
+            ['name', 'unique', 'targetClass' => '\common\models\Airport', 'message' => 'Já existe um aeroporto com esse nome.'],
+            ['website', 'unique', 'targetClass' => '\common\models\Airport', 'message' => 'Já existe um aeroporto com esse website.'],
         ];
     }
 
