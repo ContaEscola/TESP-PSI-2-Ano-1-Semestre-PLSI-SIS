980e67141ae2bea8afc04e3a97e60d2411e3d3ec
Pedro Henriques <pedrohenriques888@live.com>
2022-12-06 09:29:20 +0000

    Codigo gerado pelo gii

---
+++ aerocontrol/backend/views/restaurant/_form.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var common\models\Restaurant $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="restaurant-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'phone')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'open_time')->textInput() ?>
+
+    <?= $form->field($model, 'close_time')->textInput() ?>
+
+    <?= $form->field($model, 'logo')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'website')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ aerocontrol/backend/views/restaurant/index.php
@@ -0,0 +1,50 @@
+<?php
+
+use common\models\Restaurant;
+use yii\helpers\Html;
+use yii\helpers\Url;
+use yii\grid\ActionColumn;
+use yii\grid\GridView;
+
+/** @var yii\web\View $this */
+/** @var common\models\RestaurantSearch $searchModel */
+/** @var yii\data\ActiveDataProvider $dataProvider */
+
+$this->title = 'Restaurants';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="restaurant-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create Restaurant', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'description',
+            'phone',
+            'open_time',
+            //'close_time',
+            //'logo',
+            //'website',
+            [
+                'class' => ActionColumn::className(),
+                'urlCreator' => function ($action, Restaurant $model, $key, $index, $column) {
+                    return Url::toRoute([$action, 'id' => $model->id]);
+                 }
+            ],
+        ],
+    ]); ?>
+
+
+</div>
