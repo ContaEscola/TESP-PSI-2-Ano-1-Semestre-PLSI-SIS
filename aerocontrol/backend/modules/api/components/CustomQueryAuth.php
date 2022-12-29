abd6f69ab5f2f20c4dd6116c6d9824cb650da380
ContaEscola <2211904@my.ipleiria.pt>
2022-12-29 22:46:12 +0000

    Método de Autenticação Query atualizado

--- aerocontrol/backend/modules/api/components/CustomQueryAuth.php
+++ aerocontrol/backend/modules/api/components/CustomQueryAuth.php
@@ -1,22 +1,26 @@
 <?php
+
 namespace backend\modules\api\components;
 
 use common\models\User;
 use Yii;
 use yii\filters\auth\AuthMethod;
 use yii\web\ForbiddenHttpException;
+use yii\web\UnauthorizedHttpException;
 
-class CustomAuth extends AuthMethod
+class CustomQueryAuth extends AuthMethod
 {
     public function authenticate($user, $request, $response)
     {
         $authToken = $request->getQueryString();
+        if (empty($authToken)) throw new UnauthorizedHttpException("Failed authentication method");
+
         $token = explode('=', $authToken)[1];
         $user = User::findIdentityByAccessToken($token);
 
-        if(!$user) throw new ForbiddenHttpException('No authentication'); //403
+        if (!$user) throw new ForbiddenHttpException('No authentication'); //403
 
-        Yii::$app->params['id']=$user->id;
+        Yii::$app->params['id'] = $user->id;
         return $user;
     }
 }
