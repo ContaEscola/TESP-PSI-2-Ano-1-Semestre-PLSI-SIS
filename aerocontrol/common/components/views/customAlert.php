ae79753e6fe713d4e3da4b8caef7d59ade3456f9
ContaEscola <92809778+ContaEscola@users.noreply.github.com>
2023-01-04 09:53:12 +0000

    Add error custom alert (#46)
    * Adicionei o error ao custom alert
    
    * Fix no alert

--- aerocontrol/common/components/views/customAlert.php
+++ aerocontrol/common/components/views/customAlert.php
@@ -13,7 +13,7 @@ CustomAlertAsset::register($this);
             <use xlink:href="<?= Url::to('@web/images/' . $type . '-icon.svg#' . $type) ?>"></use>
         </svg>
     </span>
-    <p class="alert__message"><?= $body ?></p>
+    <p class="[ alert__message ] [ fs-300 letter-spacing-1 fw-bold ]"><?= $body ?></p>
     <button class="alert-toggle-btn">
         <svg class="icon alert__icon">
             <use xlink:href="<?= Url::to('@web/images/alert-close-icon.svg#alert-close-icon') ?>"></use>
