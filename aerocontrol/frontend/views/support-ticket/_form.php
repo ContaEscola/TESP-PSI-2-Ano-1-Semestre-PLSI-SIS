57ec1e730f416efdd8fafcbc8c682667e6b7a90f
pedrohenriques888 <74295605+pedrohenriques888@users.noreply.github.com>
2023-01-06 11:21:22 +0000

    Consultar suporte tickets (#52)
    * Add modal.js e correção de style.css e flight-tickets.js
    
    * adicionar modal no appAsset
    
    * Atualizar base de dados
    
    * Listar suporte ticket de cada utilizador
    
    * Atualizar o modal para criar um novo ticket
    
    * Adicionar modal funcional
    
    * Atualizar base de dados
    
    * Correção base de dados
    
    * Criar mensagem de suporte
    
    * Fix na rule da mensagem
    
    * rbac supportTicket
    
    * Fix nos css, js, _ticket, e migrei os url para o Url::to
    
    * Fix na condição para amostrar o item
    
    * Adicionar support ticket no header
    
    * Fix no dropdown do header
    
    Co-authored-by: RafaelBento7 <rafaelcarreirabento7@gmail.com>
    Co-authored-by: Pedro Miguel De Matos Norberto <2211904@my.ipleiria.pt>

---
+++ aerocontrol/frontend/views/support-ticket/_form.php
@@ -0,0 +1,59 @@
+<?php
+/** @var \frontend\models\SupportTicketForm $model */
+
+use yii\helpers\Html;
+use yii\bootstrap5\ActiveForm;
+
+?>
+
+<div class="support-ticket-form">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['create'],
+        'errorCssClass' => 'invalid',
+        'requiredCssClass' => 'invalid',
+        'successCssClass' => 'valid',
+        'validateOnType' => true,
+        'validationDelay' => 500,
+    ]); ?>
+
+    <p class="fs-400 fw-medium ">Criar um novo ticket!</p>
+
+    <div class="flow margin-top-300" data-flow-space="small">
+
+        <?= $form->field($model, 'title', [
+            'errorOptions' => [
+                'tag' => 'p',
+                'class' => 'input__error margin-top-50'
+            ],
+            'options' => ['class' => 'form__group'],
+        ])
+            ->label(null, [
+                'class' => '[ input__label ] [ fw-light margin-bottom-50 ]'
+            ])
+            ->textInput([
+                    'class' => 'form__input'
+            ]) ?>
+
+        <?= $form->field($model, 'message', [
+            'errorOptions' => [
+                'tag' => 'p',
+                'class' => 'input__error margin-top-50'
+            ],
+            'options' => ['class' => 'form__group'],
+        ])
+            ->label(null, [
+                'class' => '[ input__label ] [ fw-light margin-bottom-50 ]'
+            ])
+            ->textArea([
+                'class' => 'form__input resize-none',
+                'rows' => 7
+            ]) ?>
+
+    </div>
+    <div class="form-group">
+        <?= Html::submitButton('Guardar', ['class' => '[ button ] [ d-block fill-sm margin-top-300 push-to-right-md ]']) ?>
+    </div>
+
+    <?php ActiveForm::end();?>
+</div>
