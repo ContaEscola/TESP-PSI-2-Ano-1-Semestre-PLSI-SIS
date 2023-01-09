5aea9ee84fe923d1b1edafb469b26bd4d7d377aa
ContaEscola <2211904@my.ipleiria.pt>
2023-01-09 01:03:03 +0000

    Fix nas views do client

--- aerocontrol/backend/views/client/update.php
+++ aerocontrol/backend/views/client/update.php
@@ -5,9 +5,9 @@ use yii\helpers\Html;
 /** @var yii\web\View $this */
 /** @var common\models\Client $model */
 
-$this->title = 'Atualizar Cliente: ' . $model->user->first_name . " " . $model->user->last_name;
+$this->title = 'Atualizar Cliente: ' . $model->user->getFullName();
 $this->params['breadcrumbs'][] = ['label' => 'Clientes', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->client_id, 'url' => ['view', 'client_id' => $model->client_id]];
+$this->params['breadcrumbs'][] = ['label' => $model->user->getFullName(), 'url' => ['view', 'client_id' => $model->client_id]];
 $this->params['breadcrumbs'][] = 'Atualizar';
 ?>
 <div class="client-update">
